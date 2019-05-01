609cd4091ef232157c8962a12d468b2dcb66edec
gumpen <shuhei1993727@gmail.com>
2019-05-01 18:33:48 +0900

    Start

---
+++ 0_start/3/index.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
